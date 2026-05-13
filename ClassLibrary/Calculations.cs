afc130272c206da8a303ab09bc5627092fe3ec5f
DARKENIST <stepanhunter2007@gmail.com>
2026-05-13 21:52:53 +0700

    first commit

---
+++ ClassLibrary/Calculations.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary
+{
+    public class Calculations
+    {
+        public static string[] AvailablePeriods(TimeSpan[] startTimes, int[] durations, TimeSpan beginWorkingTime, TimeSpan endWorkingTime, int consultationTime)
+        {
+            // Защита от некорректных входных данных
+            if (consultationTime <= 0)
+                return Array.Empty<string>();
+
+            int beginMin = (int)beginWorkingTime.TotalMinutes;
+            int endMin = (int)endWorkingTime.TotalMinutes;
+
+            if (beginMin >= endMin || consultationTime > (endMin - beginMin))
+                return Array.Empty<string>();
+
+            // Формируем занятые интервалы, обрезанные рабочим днём
+            var busyIntervals = new List<(int start, int end)>();
+            for (int i = 0; i < startTimes.Length; i++)
+            {
+                int start = (int)startTimes[i].TotalMinutes;
+                int end = start + durations[i];
+
+                // Учитываем только пересечение с рабочим днём
+                if (end > beginMin && start < endMin)
+                {
+                    int clippedStart = Math.Max(start, beginMin);
+                    int clippedEnd = Math.Min(end, endMin);
+                    busyIntervals.Add((clippedStart, clippedEnd));
+                }
+            }
+
+            // Свободные непрерывные отрезки
+            List<(int start, int end)> freePeriods;
+
+            if (busyIntervals.Count == 0)
+            {
+                freePeriods = new List<(int, int)> { (beginMin, endMin) };
+            }
+            else
+            {
+                // Слияние пересекающихся интервалов
+                busyIntervals.Sort((a, b) => a.start.CompareTo(b.start));
+                var merged = new List<(int start, int end)> { busyIntervals[0] };
+
+                for (int i = 1; i < busyIntervals.Count; i++)
+                {
+                    var last = merged[merged.Count - 1];
+                    var curr = busyIntervals[i];
+                    if (curr.start <= last.end)
+                        merged[merged.Count - 1] = (last.start, Math.Max(last.end, curr.end));
+                    else
+                        merged.Add(curr);
+                }
+
+                // Построение свободных отрезков между занятыми
+                freePeriods = new List<(int start, int end)>();
+                int cur = beginMin;
+                foreach (var (mStart, mEnd) in merged)
+                {
+                    if (cur < mStart)
+                        freePeriods.Add((cur, mStart));
+                    cur = Math.Max(cur, mEnd);
+                }
+                if (cur < endMin)
+                    freePeriods.Add((cur, endMin));
+            }
+
+            // Нарезка свободных отрезков на окна длительностью consultationTime
+            var slots = new List<string>();
+            foreach (var (fs, fe) in freePeriods)
+            {
+                int pos = fs;
+                while (pos + consultationTime <= fe)
+                {
+                    int slotEnd = pos + consultationTime;
+                    slots.Add(FormatSlot(pos, slotEnd));
+                    pos += consultationTime;
+                }
+            }
+
+            return slots.ToArray();
+        }
+        
+        private static string FormatSlot(int startMin, int endMin)
+        {
+            string ToTime(int minutes) =>
+                $"{minutes / 60:D2}:{minutes % 60:D2}";
+
+            return $"{ToTime(startMin)}-{ToTime(endMin)}";
+        }
+    }
+}
---
+++ LibraryTests/Test1.cs
@@ -0,0 +1,166 @@
+﻿using ClassLibrary;
+
+namespace LibraryTests
+{
+    [TestClass]
+    public sealed class BasicTests
+    {
+        [TestMethod]
+        public void Test_EmptySchedule_ReturnsFullDaySlots()
+        {
+            var slots = Calculations.AvailablePeriods(
+                Array.Empty<TimeSpan>(),
+                Array.Empty<int>(),
+                new TimeSpan(8, 0, 0),
+                new TimeSpan(16, 0, 0),
+                30);
+            string[] expected =
+            {
+                "08:00-08:30", "08:30-09:00", "09:00-09:30", "09:30-10:00",
+                "10:00-10:30", "10:30-11:00", "11:00-11:30", "11:30-12:00",
+                "12:00-12:30", "12:30-13:00", "13:00-13:30", "13:30-14:00",
+                "14:00-14:30", "14:30-15:00", "15:00-15:30", "15:30-16:00"
+            };
+            CollectionAssert.AreEqual(expected, slots);
+        }
+
+        [TestMethod]
+        public void Test_BusyCoversWholeDay_NoFreeSlots()
+        {
+            var slots = Calculations.AvailablePeriods(
+                new[] { new TimeSpan(9, 0, 0) },
+                new[] { 540 },
+                new TimeSpan(9, 0, 0),
+                new TimeSpan(18, 0, 0),
+                30);
+            Assert.AreEqual(0, slots.Length);
+        }
+
+        [TestMethod]
+        public void Test_OnlyMorningFree_Consultation30()
+        {
+            var slots = Calculations.AvailablePeriods(
+                new[] { new TimeSpan(10, 0, 0) },
+                new[] { 90 },
+                new TimeSpan(8, 0, 0),
+                new TimeSpan(13, 0, 0),
+                30);
+            string[] expected =
+            {
+                "08:00-08:30", "08:30-09:00", "09:00-09:30", "09:30-10:00",
+                "11:30-12:00", "12:00-12:30", "12:30-13:00"
+            };
+            CollectionAssert.AreEqual(expected, slots);
+        }
+
+        [TestMethod]
+        public void Test_OnlyEveningFree_Consultation30()
+        {
+            var slots = Calculations.AvailablePeriods(
+                new[] { new TimeSpan(14, 0, 0) },
+                new[] { 60 },
+                new TimeSpan(9, 0, 0),
+                new TimeSpan(17, 0, 0),
+                30);
+            string[] expected =
+            {
+                "09:00-09:30", "09:30-10:00", "10:00-10:30", "10:30-11:00",
+                "11:00-11:30", "11:30-12:00", "12:00-12:30", "12:30-13:00",
+                "13:00-13:30", "13:30-14:00", "15:00-15:30", "15:30-16:00",
+                "16:00-16:30", "16:30-17:00"
+            };
+            CollectionAssert.AreEqual(expected, slots);
+        }
+
+        [TestMethod]
+        public void Test_GapLessThanConsultation_NoSlotInGap()
+        {
+            var slots = Calculations.AvailablePeriods(
+                new[] { new TimeSpan(10, 0, 0), new TimeSpan(10, 50, 0) },
+                new[] { 30, 30 },
+                new TimeSpan(9, 0, 0),
+                new TimeSpan(12, 0, 0),
+                30);
+            string[] expected =
+            {
+                "09:00-09:30", "09:30-10:00", "11:20-11:50"
+            };
+            CollectionAssert.AreEqual(expected, slots);
+        }
+
+        [TestMethod]
+        public void Test_GapExactlyEqualToConsultation_OneSlot()
+        {
+            var slots = Calculations.AvailablePeriods(
+                new[] { new TimeSpan(10, 0, 0), new TimeSpan(11, 0, 0) },
+                new[] { 30, 30 },
+                new TimeSpan(9, 0, 0),
+                new TimeSpan(12, 0, 0),
+                30);
+            string[] expected =
+            {
+                "09:00-09:30", "09:30-10:00", "10:30-11:00"
+            };
+            CollectionAssert.AreEqual(expected, slots);
+        }
+
+        [TestMethod]
+        public void Test_OverlappingBusyIntervals_Merged()
+        {
+            var slots = Calculations.AvailablePeriods(
+                new[] { new TimeSpan(10, 0, 0), new TimeSpan(10, 30, 0) },
+                new[] { 60, 90 },
+                new TimeSpan(9, 0, 0),
+                new TimeSpan(13, 0, 0),
+                30);
+            string[] expected =
+            {
+                "09:00-09:30", "09:30-10:00", "12:00-12:30", "12:30-13:00"
+            };
+            CollectionAssert.AreEqual(expected, slots);
+        }
+
+        [TestMethod]
+        public void Test_ConsultationLongerThanAnyFreeGap_Empty()
+        {
+            var slots = Calculations.AvailablePeriods(
+                new[] { new TimeSpan(9, 30, 0), new TimeSpan(10, 50, 0) },
+                new[] { 20, 20 },
+                new TimeSpan(9, 0, 0),
+                new TimeSpan(12, 0, 0),
+                60);
+            var slots2 = Calculations.AvailablePeriods(
+                new[] { new TimeSpan(9, 30, 0), new TimeSpan(11, 0, 0) },
+                new[] { 30, 30 },
+                new TimeSpan(9, 0, 0),
+                new TimeSpan(12, 0, 0),
+                90);
+            Assert.AreEqual(0, slots2.Length);
+        }
+
+        [TestMethod]
+        public void Test_BusyAtStartOfDay_NoSlotBefore()
+        {
+            var slots = Calculations.AvailablePeriods(
+                new[] { new TimeSpan(9, 0, 0) },
+                new[] { 30 },
+                new TimeSpan(9, 0, 0),
+                new TimeSpan(10, 0, 0),
+                30);
+            string[] expected = { "09:30-10:00" };
+            CollectionAssert.AreEqual(expected, slots);
+        }
+
+        [TestMethod]
+        public void Test_ConsultationTimeZero_EmptyResult()
+        {
+            var slots = Calculations.AvailablePeriods(
+                new[] { new TimeSpan(10, 0, 0) },
+                new[] { 30 },
+                new TimeSpan(9, 0, 0),
+                new TimeSpan(11, 0, 0),
+                0);
+            Assert.AreEqual(0, slots.Length);
+        }
+    }
+}
